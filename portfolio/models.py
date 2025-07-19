cf31d6ca9477d367066c49ad919bcef5a8cb449d
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2025-07-19 20:50:40 +0300

    completed homepage

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,14 +4,17 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class About(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150, help_text="Short professional title e.g., Full Stack Developer")
     bio = models.TextField(help_text="Main About Me content")
     profile_image = models.ImageField(upload_to='about/', blank=True, null=True)
-    cv = models.FileField(upload_to='about/cv/', blank=True, null=True, help_text="Optional CV/resume upload")
+    cv = models.FileField(upload_to='about/', blank=True, null=True, help_text="Optional CV/resume upload")
     location = models.CharField(max_length=100, blank=True)
     email = models.EmailField(blank=True)
     phone = models.CharField(max_length=20, blank=True)
+    twitter_url = models.URLField(blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 from django.views.generic import View
+from datetime import datetime
+from .models import About
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        author = About.objects.all()[0]
+        user = author.user
+        first_name = user.first_name
+        last_name = user.last_name
+        titles = author.title.split(',')
+        bio = author.bio
+        profile_image = author.profile_image
+        location = author.location
+        email = author.email
+        twitter_url = author.twitter_url
+        github_url = author.github_url
+
+        current_year = datetime.now().year
+
+        context = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'titles': titles,
+            'bio': bio,
+            'profile_image': profile_image,
+            'location': location,
+            'email': email,
+            'twitter_url': twitter_url,
+            'github_url': github_url,
+            'current_year': current_year,
+        }
+
+        print(author)
+        return render(request, 'index.html', context=context)
 
 
 class ResumeView(View):
