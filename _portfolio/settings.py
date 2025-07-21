d405e6b878b2c3ee104bb1e3021ab8ffa435022d
Abdul-Rahman Ibrahim <abdulrahmanibrahim.ish@gmail.com>
2025-07-21 22:58:15 +0300

    no

--- _portfolio/settings.py
+++ _portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-cs+l$n7bzpite3m28+r9)1gpqljh2l@f5ivcs#n^q97ilg&72z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
